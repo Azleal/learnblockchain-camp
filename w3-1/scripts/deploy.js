37f5bd635941af45a9ca1cdf6f5fc9e3f58302aa
Azleal <liyao.mr@gmail.com>
2023-03-31 10:21:44 +0800

    w3-1

--- w3-1/scripts/deploy.js
+++ w3-1/scripts/deploy.js
@@ -11,23 +11,6 @@ const {
 } = require('./MyERC20-permit')
 
 async function main() {
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const unlockTime = currentTimestampInSeconds + 60;
-
-  // const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  // const Lock = await hre.ethers.getContractFactory("Lock");
-  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  // await lock.deployed();
-
-  // console.log(
-  //   `Lock with ${ethers.utils.formatEther(
-  //     lockedAmount
-  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  // );
-
-    
     const network = await hre.ethers.provider.getNetwork()
     console.log("network:", network)
     const [owner, otherAccount] = await hre.ethers.getSigners();
